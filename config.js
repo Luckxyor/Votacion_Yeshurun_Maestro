0618fddff964017f01a80cbe5293967c3fadcd77
Luckxyor <lucaseialben2@gmail.com>
2026-03-20 03:29:38 -0300

    Cambio supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ﻿window.APP_CONFIG = {
-  supabaseUrl: "https://kirwuanvglfirzmbbwvn.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtpcnd1YW52Z2xmaXJ6bWJid3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTc2NjEsImV4cCI6MjA4OTM3MzY2MX0.b1Yh5vYd_mQMu07f8of2-SSYsvabD1H7zEEmC8nBqIo",
+  supabaseUrl: "https://dcbqsovauvxnzdgewvtz.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYnFzb3ZhdXZ4bnpkZ2V3dnR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3ODcxNjMsImV4cCI6MjA4OTM2MzE2M30.Jg8qqSQzEgMwi_b0tJh5s6Ir3J2AgwObsruE9c9i5U4",
   tables: {
     propuestas: "propuestas",
     votacion: "votacion",
